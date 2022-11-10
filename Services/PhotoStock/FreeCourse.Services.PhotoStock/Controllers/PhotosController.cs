60ae71ebd3080a132d96178d1a197b95ee6ca800
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-10 12:33:07 +0300

    PhotosController

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.PhotoStock.Dtos;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        //Cancellation tokenı neden aldık? Buraya bir fotoğraf geldiğinde örneğin 20 sn sürüyor diyelim isteği gönderen işlem tamamlanmadan iptal ederse işlem devam etmesin sonlansın diye. Asenkron başlayan bir işlemi hata fırlatarak sonlandırabilirsiniz. Cancellation Token da hata fırlatarak işlemi sonlandırır.
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo != null && photo.Length > 0)
+            {
+                //Path ini oluşturuyoruz.
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                //Photo yu kaydederken tarayıcı kapanır veya işlem sonlanırsa diye cancellation tokenı da veriyoruz. (İşlemi sonlandırması için)
+                await photo.CopyToAsync(stream, cancellationToken);
+                //İsteği yapana pathi dönüyoruz. //http://www.photostock.api.com/photos/imagesbackground.jpg
+                var returnPath = "photos/" + photo.FileName;
+                //var photoDto= new PhotoDto { Url= returnPath };
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<NoContent>.Fail("Photo is empty!",400));
+        }
+
+
+
+
+
+    }
+}
---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FreeCourse.Services.PhotoStock.Dtos
+{
+    public class PhotoDto
+    {
+        public string Url { get; set; }
+
+
+
+    }
+}
