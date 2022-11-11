6583c878f7c992d316ea64aae8091bb1f7ba4edf
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-11 19:43:06 +0300

    .

--- Services/Discount/FreeCourse.Services.Discount/Dtos/DiscountDto.cs
+++ Services/Discount/FreeCourse.Services.Discount/Dtos/DiscountDto.cs
@@ -8,7 +8,6 @@ namespace FreeCourse.Services.Discount.Dtos
         public string UserId { get; set; }
         public int Rate { get; set; }
         public string DiscountCode { get; set; }
-        public DateTime CreatedTime { get; set; }
 
 
     }
