5e7d2c67c33cd0eccad17634cc55b8cd9c78496b
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-13 13:28:09 +0300

    Entity,ValueObject,IAggreagteRoot

---
+++ Services/Order/FreeCourse.Services.Order.Domain.Core/Entity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FreeCourse.Services.Order.Domain.Core
+{
+    //Domain katmanındaki classlarımız için referans niteliğinde oluşturduk bu klasörü. Microsoft'un hazır bir kütüphanesini kullandık. Domain Driven Design da entity katmanında business kod olabilir.
+    public abstract class Entity
+    {
+        private int? _requestedHashCode;
+        private int _Id;
+
+        //İstersek ezebilelim diye virtual olarak tanımladık.
+        public virtual int Id
+        {
+            get => _Id;
+            set => _Id = value;
+        }
+
+        //Geçici mi değil mi kontrol ediyor. Default bir değeri var ise veritabanında karşılığı yoktur gibi.
+        public bool IsTransient()
+        {
+            return this.Id == default(Int32);
+        }
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode = this.Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+                return _requestedHashCode.Value;
+            }
+            else
+                return base.GetHashCode();
+        }
+
+        //İki objenin birbirine eşit olup olmadığını kontrol eder.
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Entity))
+                return false;
+
+            if (Object.ReferenceEquals(this, obj))
+                return true;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            Entity item = (Entity)obj;
+
+            if (item.IsTransient() || this.IsTransient())
+                return false;
+            else
+                return item.Id == this.Id;
+        }
+
+        //soldaki ile sağdaki itemi karşılaştırmak için kullanıyoruz.
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (Object.Equals(left, null))
+                return (Object.Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+        //soldaki ile sağdaki itemi karşılaştırmak için kullanıyoruz.
+        public static bool operator !=(Entity left, Entity right)
+        {
+            if (Object.Equals(left, null))
+                return (Object.Equals(right, null)) ? true : false;
+            else
+                return left.Equals(right);
+        }
+
+
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain.Core/ValueObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Services.Order.Domain.Core
+{
+    //Microsofttan hazır bir kütüphane kullandık.
+    public abstract class ValueObject
+    {
+        //soldaki ile sağdaki itemi karşılaştırmak için kullanıyoruz.
+        protected static bool EqualOperator(ValueObject left, ValueObject right)
+        {
+            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+            {
+                return false;
+            }
+            return ReferenceEquals(left, null) || left.Equals(right);
+        }
+
+        //soldaki ile sağdaki itemi karşılaştırmak için kullanıyoruz.
+        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+        {
+            return !(EqualOperator(left, right));
+        }
+
+        //Miras aldığımız classta bu metotu ezmemiz gerekiyor. Equals kısmında tipleri eşit olan classların içeriğini kontrol ediyoruz.
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            var other = (ValueObject)obj;
+
+            return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            return GetEqualityComponents()
+             .Select(x => x != null ? x.GetHashCode() : 0)
+             .Aggregate((x, y) => x ^ y);
+        }
+
+        public ValueObject GetCopy()
+        {
+            return this.MemberwiseClone() as ValueObject;
+        }
+
+    }
+}
