62ef39e1084d62665cbd9a85d75362ce59eda83b
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-14 19:53:40 +0300

    Authentication-Gateway.API

--- Gateways/FreeCourse.Gateway/Startup.cs
+++ Gateways/FreeCourse.Gateway/Startup.cs
@@ -1,25 +1,37 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FreeCourse.Gateway
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            //Gateway.API JWT token ile koruma altýna aldýk.
+            services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme",options=>
+            {
+                //Tokený kimin daðýttýðý bilgisini veriyoruz.
+                options.Authority = Configuration["IdentityServerURL"];
+                //Audience ý belirttik. Bu rastgele bir deðer deðil IdentityServer=>Config dosyasý üzerinden gelir.
+                options.Audience = "resource_gateway";
+                //Https bekleyeceði için onu belirttik
+                options.RequireHttpsMetadata = false;
+            });
+
             //Ocelot kütüphanesini ekledik.
             services.AddOcelot();
-
         }
 
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
