e6cf1a0dae07d1ad34bc016a485ab65601202742
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-14 18:44:52 +0300

    configuration

--- Gateways/FreeCourse.Gateway/Program.cs
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -16,8 +16,13 @@ namespace FreeCourse.Gateway
             CreateHostBuilder(args).Build().Run();
         }
 
+        //"ConfigureAppConfiguration((hostingContext,config)=> {})" tanýmladýk.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext,config)=> {
+                //Burada ekleme yapýyoruz. Docker veya local hostta ayaða kalkma durumuna göre "development" ya da "production" olarak tamamlayacak hostingContext'den.
+                config.AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName.ToLower()}.json").AddEnvironmentVariables();
+                        
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- Gateways/FreeCourse.Gateway/Startup.cs
+++ Gateways/FreeCourse.Gateway/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,29 +14,24 @@ namespace FreeCourse.Gateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //Ocelot kütüphanesini ekledik.
+            services.AddOcelot();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            //Asenkron olarak Ocelotu middleware olarak ekledik.
+            await app.UseOcelot();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
