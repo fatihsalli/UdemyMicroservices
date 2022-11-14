79bd9d1aa29fbfbb6c158bb35585f6b211919e7a
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-14 19:11:33 +0300

    Gateway-Test

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,5 +1,9 @@
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace FreeCourse.Services.Catalog
 {
@@ -7,7 +11,24 @@ namespace FreeCourse.Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            //Category yok ise category oluþturmak için seed data olarak aþaðýdaki düzenlemeyi yaptýk.
+            var host=CreateHostBuilder(args).Build();
+
+            using (var scope=host.Services.CreateScope())
+            {
+                var serviceProvider=scope.ServiceProvider;
+
+                var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+
+                if (!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto { Name = "Asp.net Core Course" }).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name = "Api Course" }).Wait();
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
