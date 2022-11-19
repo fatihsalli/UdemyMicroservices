699aeeecd2f082074bb7d3aef3b4d5e497dc2231
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-19 20:38:32 +0300

    Mvc-OrderService

---
+++ Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using FreeCourse.Web.Models.Order;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+        public OrderController(IBasketService basketService, IOrderService orderService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var basket = await _basketService.Get();
+            ViewBag.Basket=basket;
+
+            return View(new CheckoutInput());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CheckoutInput checkoutInput)
+        {
+            var orderStatus = await _orderService.CreateOrder(checkoutInput);
+
+            if (!orderStatus.IsSuccessful)
+            {
+                var basket = await _basketService.Get();
+                ViewBag.Basket = basket;
+
+                ViewBag.Error = orderStatus.Error;
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(SuccessfulCheckout), new {orderId=orderStatus.OrderId});
+        }
+
+        public IActionResult SuccessfulCheckout(int orderId)
+        {
+            ViewBag.OrderId = orderId;
+            return View();
+        }
+
+
+
+
+    }
+}
