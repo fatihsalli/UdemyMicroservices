a6ee09c193df4f17f52783c278d08c11a982f367
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-22 18:44:03 +0300

    docker-compose-test(success)

--- Services/Order/FreeCourse.Services.Order.API/Program.cs
+++ Services/Order/FreeCourse.Services.Order.API/Program.cs
@@ -1,4 +1,7 @@
+using FreeCourse.Services.Order.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace FreeCourse.Services.Order.API
@@ -7,7 +10,15 @@ namespace FreeCourse.Services.Order.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host=CreateHostBuilder(args).Build();
+            //Ayağa kalktığında migration işlemlerinin database'e yansıması için yaptık yoksa hata alırız. Ya da conatinerı her yeniden yüklediğimizde update-database dememiz gerekir Önce migration yapıyor yok ise sonrasında database 'i update ediyor.
+            using (var scope=host.Services.CreateScope())
+            {
+                var serviceProvider=scope.ServiceProvider;
+                var orderDbContext=serviceProvider.GetRequiredService<OrderDbContext>();
+                orderDbContext.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
